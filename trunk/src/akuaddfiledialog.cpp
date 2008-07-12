ba09fedc01745ae1a0d4e9046aafc88806dd631f
f0x <francesco@f0x.workgroup>
2008-07-12 19:45:30 +0200

    password function for add files

--- trunk/src/akuaddfiledialog.cpp
+++ trunk/src/akuaddfiledialog.cpp
@@ -6,12 +6,50 @@ akuAddFileDialog::akuAddFileDialog(QWidget *parent) : KDialog(parent)
    setMainWidget(mainlayout);
    KFileWidget *fw = new KFileWidget(KUrl(QDir::homePath()), mainlayout);
    fw -> setMode(KFile::Files);
-   QCheckBox *test = new QCheckBox();
-   fw -> setCustomWidget("ciao", test);
-   //mainlayout -> addWidget(fw);
-   setInitialSize(QSize(750, 400));
+
+   KVBox *layout = new KVBox();
+   layout -> setSpacing(8);
+   addwithpassword = new QCheckBox(i18n("Add file(s) with a password"), layout);
+   label = new QLabel(layout);
+   label -> setText("<font color=red>" + i18n("Password inserted") + "</font>");
+   label -> setVisible(false);
+   fw -> setCustomWidget(layout);
+   setInitialSize(QSize(700, 420));
+
+   connect(addwithpassword, SIGNAL(stateChanged(int)), this, SLOT(checkPassword(int)));
 }
 
 akuAddFileDialog::~akuAddFileDialog()
 {
+}
+
+void akuAddFileDialog::checkPassword(int state)
+{
+  // 0 = Unchecked
+  // 2 = Checked
+  if (state == 2) {
+    KNewPasswordDialog *dlg = new KNewPasswordDialog(this);
+    dlg -> setModal(true);
+    dlg -> setAllowEmptyPasswords(false);
+    dlg->setPrompt("<b>" + i18n("Enter a password") + "</b>");
+    connect(dlg, SIGNAL(newPassword(const QString&)), this, SLOT(setPassword(const QString&)));
+    connect(dlg, SIGNAL(rejected()), this, SLOT(slotCancel()));
+    dlg->show();
+  } 
+  else 
+    password.clear();
+    label -> setVisible(false);
+}
+
+void akuAddFileDialog::setPassword(const QString& newpassword)
+{  
+  password = newpassword;
+  label -> setVisible(true);
+}
+
+void akuAddFileDialog::slotCancel()
+{
+  addwithpassword -> setChecked(false);
+  password.clear();
+  label -> setVisible(false);
 }
--- trunk/src/akuaddfiledialog.h
+++ trunk/src/akuaddfiledialog.h
@@ -5,10 +5,12 @@
 #include <QCheckBox>
 
 #include <KDialog>
+#include <KVBox>
 
 #include <KDebug>
 
 #include <kfilewidget.h>
+#include <knewpassworddialog.h>
 
 #include "rarprocess.h"
 
@@ -16,9 +18,19 @@ class akuAddFileDialog : public KDialog
 {
   Q_OBJECT
 
-public:
-  akuAddFileDialog (QWidget* parent = 0);
-  ~akuAddFileDialog();
+  public:
+    akuAddFileDialog (QWidget* parent = 0);
+    ~akuAddFileDialog();
+
+  public slots:
+    virtual void checkPassword(int);
+    virtual void slotCancel();
+    virtual void setPassword(const QString&);
+
+  private: 
+    QLabel *label;
+    QString password;
+    QCheckBox *addwithpassword;
 
 };
 #endif
